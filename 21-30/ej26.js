8b677086d063ba13267d5f594b8bdd0b7ab78149
Raydberg Gabriel Chuquival Gil <raydga_2005@hotmail.com>
2025-01-17 17:53:40 -0500

    feat: add initial variables and console logs for classroom details

---
+++ 21-30/ej26.js
@@ -0,0 +1,12 @@
+let nombre = "Grafos y Arboles"
+let nroAula = 201
+let cantidadDeAlumnos = 13
+let isActivo = false
+let cantidadDeDocentes = 2
+let horario = "18:00 a 22:00"
+console.log("nombre", typeof nombre)
+console.log("nroAula", typeof nroAula)
+console.log("cantidadDeAlumnos", typeof cantidadDeAlumnos)
+console.log("isActivo", typeof isActivo)
+console.log("cantidadDeDocentes", typeof cantidadDeDocentes)
+console.log("horario", typeof horario)
